cdb4d6f6bf97cac47748f8845f751a6e91510ccb
Hanna <141652095+hannilda@users.noreply.github.com>
2023-08-28 10:56:02 -0400

    Add files via upload

---
+++ atividade3.js
@@ -0,0 +1,21 @@
+//1 forma
+
+const elemntos = ["amendoin", "pafoca", "maca"]
+const ultimoElento= elemntos.pop();
+console.log(ultimoElento); 
+console.log(elemntos); 
+//2 forma
+
+const elementos = ["amendoin","pafoca", "maca"];
+elementos2.push("geleia", "biscoito");
+console.log(elementos); 
+//3 forma
+
+const cores = ["vermelho", "verde", "azul", "amarelo"];
+const cores2 = cores.indexOf("azul");
+console.log(cores2); 
+//4 forma
+
+const palavras = ["Eu", "sou", "a", "Hanna"];
+const frase = palavras.join(" ");
+console.log(frase); 
